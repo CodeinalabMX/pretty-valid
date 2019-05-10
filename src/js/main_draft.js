bbeadc004d70ccff0d022f0d4637ce6a4bbc44ee
hEy <hey259@gmail.com>
2019-05-09 19:05:30 -0500

    Remove some test lines

--- src/js/main_draft.js
+++ src/js/main_draft.js
@@ -4,10 +4,8 @@
 $(document).ready(function()
 {
 
-	var instance = $('#form-demo').prettyValid({default_setting: 'effin\ custom'}).append('append some content to test chainability');
-	instance.public_function('some param');
-
-	var instance1 = $('#form-demo-1').prettyValid({default_setting: 'effin\ custom 1'}).append('append some content to test chainability 1');
-	instance.public_function('some param 1');
+	var instance = $('#form-demo').prettyValid({
+		default_setting: 'effin\ custom'
+	});
 
 });

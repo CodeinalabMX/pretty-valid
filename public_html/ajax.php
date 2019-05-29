d1bdd07ad77249010ec7ea308d3bab6fe5e1e996
hEy <hey259@gmail.com>
2019-05-29 14:12:42 -0500

    Add POST g_recapctha_response conditional

--- public_html/ajax.php
+++ public_html/ajax.php
@@ -1,37 +1,42 @@
 <?php
+	require '../vendor/autoload.php';
 	//* Set response content type
 	header('Content-type: application/json');
 	//* Check if is an ajax call
 	if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+
+		$handle_result = false;
+		$handle_message = 'Nothing happened.';
 		/* Get POST/GET values
 		 * eg. $_REQUEST['input_name']; */
-		$some_input = '';
-		foreach ($_POST as $key => $value) {
-			$some_input.= $key.': ' . $value . ' / ';
-		}
-		/* Validate/Sanitize data */
 		$ip = $_SERVER['REMOTE_ADDR'];
-		$g_recaptcha_response = $_POST['g-recaptcha-response'];
+		$g_recaptcha_response = (isset($_POST['g-recaptcha-response'])) ? $_POST['g-recaptcha-response'] : false;
 		$g_recaptcha_secret = '';
-		if (recaptcha_validation('localhost', 
-														 'homepage', 
-														 $g_recaptcha_secret, 
-														 $g_recaptcha_response, 
-														 $ip)) {
-			/* Handle your data
-			 * Send email, write to DB, or whatever */
-			$handle_result = true;
-		} else {
-			$handle_result = false;
-			$handle_message = 'El captcha no fue validado, intentalo nuevamente.'
-		}
 		
-		$handle_message = 'Things went ok ' . $some_input;
+		/* Validate/Sanitize data */
+		/* If a recaptcha token isset */
+		if ($g_recaptcha_response) {
+			if (recaptcha_validation('localhost', 
+															 'homepage', 
+															 $g_recaptcha_secret, 
+															 $g_recaptcha_response, 
+															 $ip)) {
+				/* Handle your data
+				 * Send email, write to DB, or whatever */
+				$handle_result = true;
+				$handle_message = 'Things worked well.';
+			} else {
+				$handle_message = 'Invalid ReCaptcha.';
+			}
+		}
+
+		
 		/* Return a response */
-		$response = array();
+		$response = [];
 		$response['status'] = $handle_result; //* Boolean
 		$response['message'] = $handle_message; //* String, optional
 		echo json_encode($response);
+
 	}
 
 	/**
@@ -45,7 +50,7 @@
 																$g_recaptcha_response, 
 																$ip)
 	{
-		$recaptcha = new \ReCaptcha\ReCaptcha($secret);
+		$recaptcha = new \ReCaptcha\ReCaptcha($g_recaptcha_secret);
 		$resp = $recaptcha->setExpectedHostname($host)
 		                  ->setExpectedAction($action)
 		                  ->setScoreThreshold(0.5)

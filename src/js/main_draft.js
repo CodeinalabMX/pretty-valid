1c856a92222bccbc9c08b4b1d35fb731b606a9d2
hEy <hey259@gmail.com>
2019-07-05 13:18:00 -0500

    Add multiple plugin instances

--- src/js/main_draft.js
+++ src/js/main_draft.js
@@ -4,7 +4,7 @@
 $(document).ready(function()
 {
 
-	var instance = $('#form-demo').prettyValid({
+	let instance = $('#form-demo').prettyValid({
 		ajax: {
 			url: 'ajax.php',
 		},
@@ -12,5 +12,17 @@ $(document).ready(function()
     	site_key: '',
   	}
 	});
+	instance.notification_show('success', 'This is a test');
+
+	let instance_1 = $('#form-demo-1').prettyValid({
+		ajax: {
+			url: 'ajax.php',
+		},
+		g_recaptcha: {
+    	site_key: '6LfVKKYUAAAAACJsoLhl_QyrCI6vNMuEg5WNcDD_',
+  	}
+	});
+
+	instance_1.notification_show('information', 'This is another test');
 
 });

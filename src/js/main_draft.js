7709b46d4db3adfe0ae4e9a8f42a319187b39954
Alejandro <A59327424@gmail.com>
2022-11-01 18:29:37 -0600

    uncomment initialization for 2 instances (forms), add recaptcha keys

--- src/js/main_draft.js
+++ src/js/main_draft.js
@@ -1,30 +1,29 @@
 /*
  * jQuery init
  */
+
 $(document).ready(function()
 {
 
-	let instance = $('#form-demo').prettyValid({
-		ajax: {
-			url: 'ajax.php',
-		},
-		g_recaptcha: {
-    	site_key: '',
-  	}
-	});
-	//instance.notification_show('success', 'This is a test');
+  let instance = $('#form-demo').prettyValid({
+    ajax: {
+      url: 'action.php',
+    },
+    g_recaptcha: {
+      site_key: '',
+    }
+  });
+  //instance.notification_show('success', 'This is a test');
 
-	/*
-	let instance_1 = $('#form-demo-1').prettyValid({
-		ajax: {
-			url: 'ajax.php',
-		},
-		g_recaptcha: {
-    	site_key: '6LfVKKYUAAAAACJsoLhl_QyrCI6vNMuEg5WNcDD_',
-  	}
-	});
 
-	instance_1.notification_show('information', 'This is another test');
-	*/
+  let instance_1 = $('#form-demo-1').prettyValid({
+    ajax: {
+      url: 'ajax.php',
+    },
+    g_recaptcha: {
+      site_key: '6LffK88iAAAAAHkbAPXojVtlFRIXzswAfFCiFAWQ',
+    }
+  });
+  //instance_1.notification_show('information', 'This is another test');
 
 });

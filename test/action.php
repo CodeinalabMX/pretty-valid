00c2ea83fd8ae47f6c979b8da73dd5d44c914c5a
Alejandro <A59327424@gmail.com>
2022-11-02 18:13:52 -0600

    Just rambling

--- test/action.php
+++ test/action.php
@@ -1,115 +1,69 @@
 <?php
 require '../vendor/autoload.php';
 
+$actions = ['submit-form-demo', 'submit-form-demo-1'];
 
-
-
-
-
-
-
-
-
-
-
-
-
-//* Set response content type
-header('Content-type: application/json');
-  //* Check if is an ajax call
-if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && 
-    strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+if (isset($_POST['action']) && in_array($_POST['action'], $actions) ) {
 
   $handle_result = false;
-  $handle_message = 'Nothing happened.';
-  
-  $g_recaptcha_response = false;
-  if (isset($_POST['g-recaptcha-response'])) {
+
+  if (!empty(isset($_POST['g-recaptcha-response'])) {
     $g_recaptcha_response = $_POST['g-recaptcha-response'];
     $ip = $_SERVER['REMOTE_ADDR'];
     $hostname = $_SERVER['HTTP_HOST'];
-
-  }
-
-  //* Validate recaptcha if token isset
-  if ($g_recaptcha_response) {
-    if (g_recaptcha_validation($g_recaptcha_response, 
-      $hostname, 
-      $ip)) {
-      $handle_result = true;
-      $handle_message = 'reCaptcha validation success.';
-    } else {
+    if (!g_recaptcha_validation($g_recaptcha_response, $hostname, $ip)) {
       $handle_message = 'reCaptcha validation error.';
     }
   }
 
-  //* Validate/Sanitize POST data
-  if () {
+  //* Sanitize/Validate POST data
 
+
+  if (!empty($_POST['is-ajax'])) {
+    header('Content-type: application/json');
+    $response = [];
+    $response['status'] = $handle_result; //* Boolean
+    $response['message'] = $handle_message; //* String, optional
+    echo json_encode($response);
   }
-
-  /* Return a response */
-  $response = [];
-  $response['status'] = $handle_result; //* Boolean
-  $response['message'] = $handle_message; //* String, optional
-  echo json_encode($response);
 
 }
 
 
-    /**
-     * Google Recaptcha Validation
-     *
-     * @return boolean
-     */
-    function g_recaptcha_validation($g_recaptcha_response, $hostname, $ip_address)
-    {
-      $g_recaptcha_secret_key = '';
-      /* If allow_url_fopen is off in production server 
-         Enable recaptcha using curl method
-         ref: https://stackoverflow.com/a/62086095 */
-      $g_recaptcha = new \ReCaptcha\ReCaptcha($g_recaptcha_secret_key, 
-                     new \ReCaptcha\RequestMethod\CurlPost());
-      $response = $g_recaptcha->setExpectedHostname($hostname)
-      ->setScoreThreshold(0.5)
-      ->verify($g_recaptcha_response, $ip_address);
-      if ($response->isSuccess())
-      {
-        $this->result_code = 1;
-        return true;
-      }
-      else
-      {
-        $this->result_code = 2;
-        $this->result_message = $response->getErrorCodes();
-        return false;
-      }
-    }
+function form_validation()
+{
+
+  //* Validate/Sanitize POST data
+  return true;
+}
 
 
-  /**
-   * reCaptcha validation
-   *
-   * Server-side verification
-   * https://github.com/google/recaptcha
-   *
-   * @return boolean
-   */
-  function recaptcha_validation($host, 
-    $action, 
-    $g_recaptcha_secret, 
-    $g_recaptcha_response, 
-    $ip)
-  {
-    $recaptcha = new \ReCaptcha\ReCaptcha($g_recaptcha_secret);
-    $resp = $recaptcha->setExpectedHostname($host)
-    ->setExpectedAction($action)
-    ->setScoreThreshold(0.5)
-    ->verify($g_recaptcha_response, $ip);
-    if ($resp->isSuccess()) {
+/**
+ * Google Recaptcha Validation
+ * https://github.com/google/recaptcha
+ *
+ * @return boolean
+ */
+function g_recaptcha_validation($g_recaptcha_response, $hostname, $ip_address)
+{
+  $g_recaptcha_secret_key = '';
+  /* If allow_url_fopen is off in production server 
+     Enable recaptcha using curl method
+     ref: https://stackoverflow.com/a/62086095 */
+     $g_recaptcha = new \ReCaptcha\ReCaptcha($g_recaptcha_secret_key, 
+       new \ReCaptcha\RequestMethod\CurlPost());
+     $response = $g_recaptcha->setExpectedHostname($hostname)
+     ->setScoreThreshold(0.5)
+     ->verify($g_recaptcha_response, $ip_address);
+     if ($response->isSuccess())
+     {
+      $this->result_code = 1;
       return true;
-    } else {
-      $errors = $resp->getErrorCodes();
+    }
+    else
+    {
+      $this->result_code = 2;
+      $this->result_message = $response->getErrorCodes();
       return false;
     }
   }
